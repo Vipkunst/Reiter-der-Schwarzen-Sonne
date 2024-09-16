c5ce3a76b18a205066ae874b5de77e9e0c42d4fd
Vipkunst <max.bystrov10@gmail.com>
2024-09-16 12:29:56 +0200

    .

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ sek_read = 1
 total_sections = len(sektionen.hauptpfad)
 
 for sek in sektionen.hauptpfad:
-    fortschritt = sek_read / total_sections * 100
-    print(f"Progress: {fortschritt:.2f}%")
+    progress = sek_read / total_sections * 100
+    print(f"Progress: {progress:.2f}%")
     print(sek)
     sek_read += 1
     input()
