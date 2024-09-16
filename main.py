1196fa355e8d39851b82d8861013cd8338ccd580
giustu78 <giulia.stuermer@gmail.com>
2024-09-16 13:46:05 +0200

    Konsole clearen

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sektionen
+import os
 
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 sek_read = 1
 total_sections = len(sektionen.hauptpfad)
 
 for sek in sektionen.hauptpfad:
+    clear_console()
     progress = sek_read / total_sections * 100
     print(f"Progress: {progress:.2f}%")
     print(sek)
     sek_read += 1
     input()
+    print("hallo")
