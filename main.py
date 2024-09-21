fcec7aba728fa991e366c97bd990a564ddc1e4fc
Jonas <99035978+Johnny-tech-droid@users.noreply.github.com>
2024-09-21 17:24:31 +0200

    textwrap implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sektionen
 import os
-
+import textwrap
 # Print all sections
 def clear_console():
     if os.name == 'nt':
@@ -13,9 +13,10 @@ def iterate_all_sections():
     sek_read = 0
     total_sections = len(sektionen.erster_prototyp_pfad)
     for sek in sektionen.erster_prototyp_pfad:
+        wrapped_text = textwrap.fill(sek, width=100)
         clear_console()
         progress = sek_read / total_sections * 100
-        print(sek)
+        print(wrapped_text)
         print(f"Progress: {progress:.2f}%")
         sek_read += 1
         input("Enter to continue")
