8a8b8d0a5b455b4fae79797ff16f96b4de848689
Jonas <99035978+Johnny-tech-droid@users.noreply.github.com>
2024-09-22 18:31:12 +0200

    textwrap implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sektionen
-import os
+import individual
+import content
 import textwrap
+import os
 # Print all sections
 def clear_console():
     if os.name == 'nt':
@@ -9,20 +10,56 @@ def clear_console():
         os.system('clear')
 
 # Iterate over all sections
-def iterate_all_sections():
-    sek_read = 0
-    total_sections = len(sektionen.erster_prototyp_pfad)
-    for sek in sektionen.erster_prototyp_pfad:
-        wrapped_text = textwrap.fill(sek, width=100)
-        clear_console()
-        sek_read += 1
-        progress = sek_read / total_sections * 100
-        print(wrapped_text)
-        print(f"Progress: {progress:.2f}%")
-        sek_read += 1
-        input("Enter to continue")
+def show_default_section():
+    section_read = 0
+    total_sections = len(content.erster_prototyp_pfad)
+    for contents in content.erster_prototyp_pfad:
+        wrapped_section = textwrap.fill(contents, width=100)
+        print(wrapped_section)
+        section_read += 1
+        print(f"Progress: {section_read / total_sections * 100:.2f}%")
+        print("\n")
+        input("Press Enter to continue...")
 
 
-iterate_all_sections()
+def show_section(section_key):
+    section = individual.chapter[section_key]
+    wrapped_section = textwrap.fill(section['content'], width=100)
+    print(wrapped_section)
+
+    for key, value in section['text_options'].items():
+        print(f"{key}: {value}")
+    return section['options']
 
 
+def init_game():
+    current_section = 'abs1'
+    while True:
+        options = show_section(current_section)
+        user_input = get_user_input()
+
+        if user_input in options:
+            current_section = options[user_input]
+        else:
+            print("Invalid section number. Please enter a valid section number.")
+
+
+
+def choose_paths():
+    user_choice_path = input("Wähle deinen Pfad: \n1.Individueller Pfad\n2.Standard Pfad\nAuswahl: ")
+    if user_choice_path == "1":
+        init_game()
+    elif user_choice_path == "2":
+        show_default_section()
+    else:
+        print("Invalid input. Please enter a valid input.")
+        choose_paths()
+
+def main():
+    print("\n")
+    choose_paths()
+
+
+
+if __name__ == "__main__":
+   main()
